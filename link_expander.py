fba75ab8799067a19633e0f208147515bb9e2794
Ali Fisher <the.information.horizon@gmail.com>
2013-06-27 10:26:24 +0100

    Update link_expander.py

--- link_expander.py
+++ link_expander.py
@@ -1,10 +1,10 @@
-
+from urlunshort import resolve
 import httplib
 import urlparse
 import csv
 
 #list of shortlinks:
-url = ['http://t.co/uBFlnqZ0YU'
+urls = ['http://zite.to/12Uq1nW'
 ]
 
 #store file
@@ -12,23 +12,12 @@ outfile = 'longlinks'".csv"
 myfile = open(outfile, "wb",)
 w = csv.writer(myfile)
 
-def unshorten_url(url):
-    parsed = urlparse.urlparse(url)
-    h = httplib.HTTPConnection(parsed.netloc)
-    resource = parsed.path
-    if parsed.query != "":
-        resource += "?" + parsed.query
-    h.request('HEAD', resource )
-    response = h.getresponse()
-    if response.status/100 == 3 and response.getheader('Location'):
-        return unshorten_url(response.getheader('Location')) # changed to process chains of short urls
-    else:
-        return url
 
-for row in [url]:
-    print expand-shorlinks.unshorten_url()
 
+for url in urls:
+    results = []
+    results += [resolve(url)]
+    w.writerow(results)
 
 myfile.close()
-
 
